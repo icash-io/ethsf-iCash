333fcc7216bcdea3f2577f8e65d5eb220bc3f01c
Kent <khmakishima@ucdavis.edu>
2018-10-06 14:11:34 -0700

    changes

--- frontend/iCash/js/controllers.js
+++ frontend/iCash/js/controllers.js
@@ -1,9 +1,20 @@
 angular.module('app.controllers', [])
-  
+
+import { Injectable } from ‘@angular/core’;
+import * as Web3 from ‘web3’;
+import * as TruffleContract from ‘truffle-contract’;
+
+//GET https://www.coinbase.com/oauth/authorize?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URL&state=SECURE_RANDOM&scope=wallet:accounts:read
+
+
 .controller('payCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+
+// This uses the credit card
+
+// This calls 
 
 
 }])
@@ -13,6 +24,7 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+// 
 
 }])
    
@@ -21,6 +33,7 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+//
 
 }])
       
@@ -29,6 +42,7 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+//
 
 }])
    
@@ -37,6 +51,7 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+//Login Info
 
 }])
    
@@ -45,6 +60,7 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+//
 
 }])
  
